c958e71a1b1b0355aef55f9a223b3b76cdf69fd4
Nilton Nascimento <nilton.nascimento@kehe.com>
2023-06-12 00:33:56 -0300

    Add tools (disabled until proper configuration)

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: unused-local
 -- Read the docs: https://www.lunarvim.org/docs/configuration
 -- Example configs: https://github.com/LunarVim/starter.lvim
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
@@ -38,5 +39,31 @@ lvim.plugins = {
       vim.api.nvim_set_keymap("n", "s", ":HopWord<cr>", { silent = true })
     end,
   },
+  { 'tpope/vim-dadbod' },
+  { 'kristijanhusak/vim-dadbod-ui' },
+  { 'nvim-treesitter/playground' },
+  { 'theprimeagen/harpoon', enabled = false
+    -- opts = function(_, opts)
+    --   local mark = require("harpoon.mark")
+    --   local ui = require("harpoon.ui")
+    --   local tmux = require("harpoon.tmux")
+
+    --   vim.api.nvim_set_keymap("n", "<leader>a", mark.add_file)
+    --   vim.api.nvim_set_keymap("n", "<C-e>", ui.toggle_quick_menu)
+
+    --   vim.api.nvim_set_keymap("n", "<C-a>", function() ui.nav_file(1) end)
+    --   vim.api.nvim_set_keymap("n", "<C-s>", function() ui.nav_file(2) end)
+    --   vim.api.nvim_set_keymap("n", "<C-d>", function() ui.nav_file(3) end)
+    --   vim.api.nvim_set_keymap("n", "<C-f>", function() ui.nav_file(4) end)
+    --   vim.api.nvim_set_keymap("n", "<C-g>", function() ui.nav_file(5) end)
+
+    --   vim.api.nvim_set_keymap("n", "<S-a>", function() tmux.sendCommand(1, "pwd") end)
+    --   vim.api.nvim_set_keymap("n", "<S-s>", function() tmux.sendCommand(2, "pwd") end)
+    --   vim.api.nvim_set_keymap("n", "<S-d>", function() tmux.sendCommand(3, "pwd") end)
+    --   vim.api.nvim_set_keymap("n", "<S-f>", function() tmux.sendCommand(4, "pwd") end)
+    --   vim.api.nvim_set_keymap("n", "<S-g>", function() tmux.sendCommand(5, "pwd") end)
+    -- end,
+  },
+  { 'tpope/vim-fugitive', enabled = false },
 }
 
