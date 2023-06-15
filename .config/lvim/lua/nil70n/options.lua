fca641ffaeb29b72f9a7ad281a8e757aae1dde7a
Nilton Nascimento <nilton.nascimento@kehe.com>
2023-06-15 20:05:30 -0300

    Fixing some lunarvim mappings

---
+++ .config/lvim/lua/nil70n/options.lua
@@ -0,0 +1,8 @@
+-- Vim options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.g.db_ui_save_location = '~/.config/db_ui'
+
+-- LunarVim options
+lvim.transparent_window = true
+lvim.colorscheme = 'rose-pine-main'
